8137fd393cd983eff298b32cc81cd493e7c685e6
Magi <paoloimbrmagi@gmail.com>
2024-12-04 22:01:21 +0100

    Added Day4 (incomplete)

--- 2024/Day4/part1.cpp
+++ 2024/Day4/part1.cpp
@@ -1,3 +1,4 @@
+#include <cmath>
 #include <fstream>
 #include <string>
 #include <sstream>
@@ -5,6 +6,12 @@
 #include <regex>
 #include <iostream>
 using namespace std;
+
+// Take the point where the X is and checks if the word XMAS is in the matrix
+int findXMASHorizontal(int, int, vector<string>);
+int findXMASVertical(int, int, vector<string>);
+int findXMASDiagonalRight(int, int, vector<string>);
+int findXMASDiagonalLeft(int, int, vector<string>);
 
 int main(int argc, char* argv[]) {
 
@@ -21,33 +28,160 @@ int main(int argc, char* argv[]) {
         exit(1);
     }
 
-    vector< vector<char> > mat;
+    // vector of strings or a vector of a vector of chars
+    // char* argv[] is a vector of strings
+    vector<string> ws;
+    int countXMAS = 0;
     while(true)
     {
     // Finche' non arrivo alla fine del file
         if(!input.eof()) {
-            vector<char> row;
+            string row;
             char value;
             // Inserisco valori nella matrice ...
-            while(input >> value) {
-                row.push_back(value);
-                // Finché non arrivo a capo
-                if(input.get() == '\n') break;
-            }
-            // Se il vettore non e' vuoto, lo inserisco nella matrice
+            getline(input, row);
+
             if(!row.empty())
-                mat.push_back(row);
+                ws.push_back(row);
 
         } else {
             break;
         }
     }
 
-    for(int i = 0; i < mat.size(); i++) {
-        for(int j = 0; j < mat[i].size(); j++) {
-            cout << mat[i][j] << " ";
+    // Prints char by char
+    for(int i = 0; i < ws.size(); i++) {
+        for(int j = 0; j < ws[i].size(); j++) {
+            cout << ws[i][j] << " ";
         }
         cout << endl;
     }
 
+    int sol = 0;
+
+    for(int i = 0; i < ws.size(); i++) {
+        for(int j = 0; j < ws[i].size(); j++) {
+            if(ws[i][j] == 'X') {
+                sol += findXMASVertical(i, j, ws) + findXMASHorizontal(i, j, ws)
+                    + findXMASDiagonalLeft(i, j, ws) + findXMASDiagonalRight(i, j, ws);
+            }
+        }
+    }
+
+    cout << "Soluzione: " << sol << endl;
+
+    return 0;
+}
+
+// Takes the point where the X is and checks if the word XMAS is in the matrix
+int findXMASHorizontal(int x, int y, vector<string> ws) {
+    int countXMAS = 0;
+    string wordtoCheck = "";
+
+    // Check the horizontal
+    for(int move = -3; move <= 3; move++) {
+        // If its out of bounds, we dont want to append
+        // If its in bounds, we want to append
+        if(y + move >= 0 && y + move < ws[x].size()) {
+            wordtoCheck += ws[x][y + move];
+        }
+    }
+
+    cout << wordtoCheck << endl;
+
+    // Checks if the word contains XMAS
+    if(wordtoCheck.find("XMAS") != string::npos) {
+        countXMAS++;
+    }
+
+    // Checks if the word contains SAMX
+    if(wordtoCheck.find("SAMX") != string::npos) {
+        countXMAS++;
+    }
+
+    return countXMAS;
+}
+
+// Takes the point where the X is and checks if the word XMAS is in the matrix
+int findXMASVertical(int x, int y, vector<string> ws) {
+    int countXMAS = 0;
+    string wordtoCheck = "";
+
+    // Check the vertical
+    for(int move = -3; move <= 3; move++) {
+        // If its out of bounds, we dont want to append
+        // If its in bounds, we want to append
+        if(x + move >= 0 && x + move < ws.size()) {
+            wordtoCheck += ws[x + move][y];
+        }
+    }
+
+    cout << wordtoCheck << endl;
+
+    if(wordtoCheck.find("XMAS") != string::npos) {
+        countXMAS++;
+    }
+
+    if(wordtoCheck.find("SAMX") != string::npos) {
+        countXMAS++;
+    }
+
+    return countXMAS;
+}
+
+// Takes the point where the X is and checks if the word XMAS is in the matrix
+int findXMASDiagonalLeft(int x, int y, vector<string> ws) {
+    int countXMAS = 0;
+    string wordtoCheck = "";
+
+    // Check the diagonal left
+    for(int move = -3; move <= 3; move++) {
+        // If its out of bounds, we dont want to append
+        // If its in bounds, we want to append
+        if(x + move >= 0 && y + move >= 0 && x + move < ws.size() && y + move < ws[x].size()) {
+            wordtoCheck += ws[x + move][y + move];
+        }
+    }
+
+
+    cout << wordtoCheck << endl;
+
+    if(wordtoCheck.find("XMAS") != string::npos) {
+        countXMAS++;
+
+    }
+
+    if(wordtoCheck.find("SAMX") != string::npos) {
+        countXMAS++;
+    }
+
+    return countXMAS;
+}
+
+// Takes the point where the X is and checks if the word XMAS is in the matrix
+int findXMASDiagonalRight(int x, int y, vector<string> ws) {
+    int countXMAS = 0;
+    string wordtoCheck = "";
+
+    // Check the diagonal right
+    for(int move = 3; move >= -3; move--) {
+        // If its out of bounds, we dont want to append
+        // If its in bounds, we want to append
+        if(x + move >= 0 && y + move >= 0 && x + move < ws.size() && y + move < ws[x].size()) {
+            wordtoCheck += ws[x + move][y + move];
+        }
+    }
+
+    cout << wordtoCheck << endl;
+
+    if(wordtoCheck.find("XMAS") != string::npos) {
+        countXMAS++;
+
+    }
+
+    if(wordtoCheck.find("SAMX") != string::npos) {
+        countXMAS++;
+    }
+
+    return countXMAS;
 }
